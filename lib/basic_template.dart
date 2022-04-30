3bb44feb3a479bf45919089cc3faa9bd7c3cde65
lubshad <lubshad4u4@gmail.com>
2022-04-30 11:45:18 +0530

    delete with id added

--- lib/basic_template.dart
+++ lib/basic_template.dart
@@ -12,7 +12,6 @@ export "/packages/animate_route_builder.dart";
 export '/packages/formatters.dart';
 export '/packages/route_utils.dart';
 export 'package:dartz/dartz.dart' hide State;
-export '/packages/file_upload.dart';
 export 'package:get/get.dart' hide MultipartFile, Response;
 export 'package:http/http.dart'
     show Client, MultipartFile, MultipartRequest, Response;
--- lib/packages/usecase.dart
+++ lib/packages/usecase.dart
@@ -1,7 +1,29 @@
 
-import 'package:basic_template/packages/app_error.dart';
-import 'package:dartz/dartz.dart';
+
+import '../basic_template.dart';
 
 abstract class UseCase<Type, Params> {
   Future<Either<AppError, Type>> call(Params params);
+}
+
+
+class UploadFileParams {
+  final Map<String, dynamic> data;
+  final List<MultipartFile> files;
+  final String path;
+
+  UploadFileParams(this.data, this.files, this.path);
+}
+
+class DeleteWithID {
+  final int id;
+
+  DeleteWithID(this.id);
+
+  toJson() {
+    return {
+      "id": id,
+    };
+  }
+  
 }
