cce71f96efa9e8d967b785744afc091d1e53934a
lubshad <lubshad4u4@gmail.com>
2022-06-24 12:21:57 +0530

    api constants changed

--- app_template/lib/data/core/api_constants.dart
+++ app_template/lib/data/core/api_constants.dart
@@ -4,17 +4,11 @@ class ApiConstants {
   ApiConstants._();
   // local
   static String get domainUrl =>
-      kDebugMode ? "http://localhost" : "http://143.244.138.169";
+      kDebugMode ? "http://localhost" : "http://domainUrlHere";
 
   static String get baseUrl =>
       kDebugMode ? "$domainUrl:8000$slugUrl" : domainUrl + slugUrl;
-
-  static String presentationEditorUrl =
-      kDebugMode ? "$domainUrl:8002/" : "$domainUrl/presentation/";
-
-  static String presentationViewerUrl =
-      kDebugMode ? "$domainUrl:8003/" : "$domainUrl/presentation_view/";
-  static const String slugUrl = "/folldy_admin/api/";
+  static const String slugUrl = "/slug_url_here";
 }
 
 getImageUrl(String url) {
