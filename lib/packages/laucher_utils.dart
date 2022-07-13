ab834c2e032ba4a5ad38c8ab0653ba9ae982ca03
lubshad <lubshad4u4@gmail.com>
2022-07-13 12:55:29 +0530

    both apps dep update

--- lib/packages/laucher_utils.dart
+++ lib/packages/laucher_utils.dart
@@ -1,9 +1,9 @@
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> launchInBrowser(Uri url) async {
-  if (!await launchUrl(
-    url,
-  )) {
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url, mode: LaunchMode.externalApplication);
+  } else {
     throw 'Could not launch $url';
   }
 }
