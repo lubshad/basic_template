fbcd247a86fb7020816a8eaffc42ea61329c6929
lubshad <lubshad4u4@gmail.com>
2022-11-05 23:42:08 +0530

    ignore fix

--- lib/basic_template.dart
+++ lib/basic_template.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: depend_on_referenced_packages
+
 library basic_template;
 
 export 'package:logging/logging.dart';
@@ -24,17 +26,17 @@ export 'package:flutter_svg/flutter_svg.dart';
 export 'package:equatable/equatable.dart';
 export 'package:google_fonts/google_fonts.dart';
 export 'package:animations/animations.dart';
-// export 'package:rxdart/rxdart.dart' hide Subject;
+export 'package:rxdart/rxdart.dart' hide Subject;
 export 'package:flutter_cache_manager/flutter_cache_manager.dart';
 export 'package:firebase_core/firebase_core.dart';
 export 'package:permission_handler/permission_handler.dart';
 export 'package:path_provider/path_provider.dart';
 export 'package:get_storage/get_storage.dart';
 export 'package:sqflite/sqflite.dart';
-// export 'package:path/path.dart';
+export 'package:path/path.dart';
 export 'package:package_info_plus/package_info_plus.dart';
 export 'package:file_picker/file_picker.dart';
-// export 'package:http_parser/http_parser.dart';
+export 'package:http_parser/http_parser.dart';
 export 'package:shimmer/shimmer.dart';
 export 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 export 'package:share_plus/share_plus.dart';
