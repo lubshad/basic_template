37ea94c435045ec4d44354644c377842de59801c
lubshad <lubshad4u4@gmail.com>
2023-08-05 16:17:27 +0530

    retry removed from exception

--- lib/packages/repository_exception.dart
+++ lib/packages/repository_exception.dart
@@ -8,31 +8,32 @@ mixin RepositoryExceptionMixin {
   Future<Either<AppError, T>> exceptionHandler<T>(
     FutureOr computation,
   ) async {
-    const maxRetries = 60;
-    for (int retry = 0; retry < maxRetries; retry++) {
-      try {
-        final result = await computation;
-        return Right(result);
-      } on SocketException catch (e) {
-        switch (e.osError?.errorCode) {
-          // case 54:
-          // case 60:
-          //   await Future.delayed(const Duration(seconds: 1));
-          //   logInfo(
-          //       "error Code ${e.osError?.errorCode}   retrying....($retry)");
-          //   continue;
-          default:
-            logError("Check Network Connection $e");
-            return const Left(AppError(AppErrorType.network));
-        }
-      } on ApiException catch (e) {
-        logError("Something went wrong $e");
-        return Left(AppError(AppErrorType.api, message: e.toString()));
-      } on UnauthorizedException catch (e) {
-        logInfo("Unauthorized  $e");
-        return Left(AppError(AppErrorType.unauthorised, message: e.toString()));
+    // const maxRetries = 3;
+    // for (int retry = 0; retry < maxRetries; retry++) {
+    try {
+      final result = await computation;
+      return Right(result);
+    } on SocketException catch (e) {
+      switch (e.osError?.errorCode) {
+        // case 54:
+        // case 60:
+        //   // await Future.delayed(const Duration(seconds: 1));
+        //   // logInfo("retrying....($retry)");
+        //   // continue;
+        //   break;
+        default:
+          logError("Check Network Connection ${e.osError?.message}");
+          return Left(
+              AppError(AppErrorType.network, message: e.osError?.message));
       }
+    } on ApiException catch (e) {
+      logError("Something went wrong $e");
+      return Left(AppError(AppErrorType.api, message: e.toString()));
+    } on UnauthorizedException catch (e) {
+      logInfo("Unauthorized  $e");
+      return Left(AppError(AppErrorType.unauthorised, message: e.toString()));
     }
-    return const Left(AppError(AppErrorType.network));
+    // }
+    // return const Left(AppError(AppErrorType.network));
   }
 }
